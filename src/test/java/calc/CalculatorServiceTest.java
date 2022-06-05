64c85161a30b954b8562f930c3b9a1e487d09a31
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-06-05 23:51:26 +0200

    WIP

---
+++ src/test/java/calc/CalculatorServiceTest.java
@@ -0,0 +1,44 @@
+package calc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceTest {
+
+    public CalculatorService sut = new CalculatorService();
+
+    @Test
+    public void shouldWorkWithEmptyString() {
+        Assertions.assertEquals( 0, sut.evaluate(""));
+    }
+
+    @Test
+    public void shouldParseNumbers() {
+        Assertions.assertEquals( 3, sut.evaluate("3"));
+    }
+
+    @Test
+    public void shouldParseFloatNumbers() {
+        Assertions.assertEquals(3.5, sut.evaluate("3.5"));
+    }
+
+    @Test
+    public void shouldSupportAddition() {
+        Assertions.assertEquals(4, sut.evaluate("1 3 +"));
+    }
+
+    @Test
+    public void shouldSupportMultiplication() {
+        Assertions.assertEquals(3, sut.evaluate("1 3 *"));
+    }
+
+    @Test
+    public void shouldSupportSubtraction() {
+        Assertions.assertEquals(-2, sut.evaluate("1 3 -"));
+    }
+
+    @Test
+    public void shouldSupportDivision() {
+        Assertions.assertEquals(2, sut.evaluate("4 2 /"));
+    }
+}
